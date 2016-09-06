8c2fa126cb68a77e721051e70cf159ddb01485da
melbandit <melissa.i.sattler@gmail.com>
2016-09-06 16:59:28 -0400

    updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,21 @@
+$( ".cross-bkg" ).hide();
 $( ".cross" ).hide();
+$('.closed').hide();
 
 $( "#menu" ).click(function() {
 	$( "#menu" ).slideToggle( "slow", function() {
 		$( "#menu" ).hide();
+		$( ".cross-bkg" ).show();
 		$( ".cross" ).show();
+		$('.closed').show();
 	});
 });
 
 $( ".cross" ).click(function() {
-	$( "#menu" ).slideToggle( "slow", function() {
+	$( "#menu" ).slideToggle( "fast", function() {
+		$( ".cross-bkg" ).hide();
 		$( ".cross" ).hide();
 		$( "#menu" ).show();
+		$('.closed').hide();
 	});
 });
